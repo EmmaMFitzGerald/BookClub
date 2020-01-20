7530ebce2914d40adbca569ac86227c029f76a23
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-01-20 17:12:21 -0500

    Users new & create controllers added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
 
     def new 
-        @user = User.new
+        @user = User.new #creates new instance of a user
     end 
 
     def create
+        @user = User.new(user_params)
+        if @user.save #the user has passed validations and can be created & logged in
+            session[:user_id] = @user.id #creates a new session with that users id
+            redirect_to @user #takes the user to their show page
+        else #the user has not passed validations and needs to reenter information 
+            render :new
+        end 
     end 
 
     def show
     end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password) #determines what information can be sent through the form
+    end 
 end
+
