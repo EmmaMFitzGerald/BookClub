02402009a2dc2a6a5be3c3a68374f62a2f7a091e
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-01-21 11:44:55 -0500

    edit comment action and controller added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:edit, :update]
+
     def index
         if params[:book_id] && @book = Book.find_by_id(params[:book_id])
         #checks if the route is nested by determing the book_id then sets the book
@@ -13,7 +15,7 @@ class CommentsController < ApplicationController
             #checks if the route is nested by determing the book_id then sets the book
             @comment = @book.comments.build
         else 
-            @error = "This book doesn't exist" if params[:book_id]
+          @error = "This book doesn't exist" if params[:book_id]
             @comment = Comment.new
         end 
     end 
@@ -29,19 +31,31 @@ class CommentsController < ApplicationController
 
     def show 
         @book = Book.find_by_id(params[:book_id])
-        @comments = @book.comments
     end 
 
     def edit
-        @comment = Comment.find_by(id: params[:id])
+        @comment = Comment.find(params[:id])
+        @book = @comment.book
     end 
 
     def update
-        @comment = Comment.find_by(id: params[:id])  
+        if @comment.update(comment_params) 
+            redirect_to book_comments_path(@comment.book.id) #pass in the book id as an argument
+        else
+            render :edit
+        end
     end 
 
     private
     def comment_params
         params.require(:comment).permit(:chapter, :content, :book_id) #specify the only information we want to allow in
     end 
+
+    def set_comment
+        @comment = Comment.find_by(id: params[:id])
+        if !@comment
+          flash[:message] = "Comment was not found"
+          redirect_to comments_path
+        end
+    end
 end
