0fce3217be86feeb19818606bd7c30a48842462f
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-01-21 07:51:32 -0500

    added feature to create a new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,20 @@
 class BooksController < ApplicationController
+
+    def new
+        @book = Book.new
+    end 
+
+    def create
+        @book = current_user.books.build(book_params) #book created belongs to the the current user
+        if @book.save 
+            redirect_to books_path #if the book saves there haven't been any errors, so can show all the books
+        else 
+            render :new #have to retry entering information
+        end 
+    end 
+
+    private
+    def book_params
+        params.require(:book).permit(:title, :author, :genre, :blurb) #specify the only information we want to allow in
+    end 
 end
