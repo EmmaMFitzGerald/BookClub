0aaae8110c8cd62d72b64278f871cefb102f0391
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-01-21 12:24:45 -0500

    delete book capability added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,25 @@ class BooksController < ApplicationController
         @book = Book.find_by(id: params[:id])
     end 
 
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update 
+        @book = Book.find_by(id: params[:id])
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else
+         render :edit
+        end
+    end 
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end 
+
     private
     def book_params
         params.require(:book).permit(:title, :author, :genre, :blurb) #specify the only information we want to allow in
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, :dependent => :destroy #deletes comments associated with a book
   has_many :users, through: :comments
 end
