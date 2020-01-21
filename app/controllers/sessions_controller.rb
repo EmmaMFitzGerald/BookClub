37f9ffb7281858fa0209b9197d1f5b2bfd8fc272
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-01-21 06:52:00 -0500

    login page, and actions added and working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,14 @@ class SessionsController < ApplicationController
         redirect_to root_path #redirect to homepage once logged out
     end
 
-    
-
+    def create
+        @user = User.find_by(username: params[:user][:username])#look for the user based on username
+        if @user && @user.authenticate(params[:user][:password])#if we find the user AND the password is right
+            session[:user_id] = @user.id #create a new session id
+            redirect_to user_path(@user) #bring the user to their show page
+        else 
+            flash[:message] = "Incorrect info, please try again!"
+            redirect_to '/login' #rerender log-in page
+        end 
+    end 
 end
