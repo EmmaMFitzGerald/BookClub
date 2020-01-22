00faa2ab034cf665a43f21fa51fb2f27fd4d0d43
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-01-22 15:48:57 -0500

    commiting before deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_20_213531) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
